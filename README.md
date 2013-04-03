c913d27be9571ba5a847644ab7cb28d235317b66
Matt Hintzke <mhintzke.wsu@hotmail.com>
2013-04-03 15:14:23 -0700

    Trying to do mkdir... dont get the dev part

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Mon Apr  1 21:06:28 PDT 2013 - Added a save.sh shell script to push changes to g
 
 Mon Apr  1 21:12:16 PDT 2013 - Created the log.sh file to assist README.md logging
 Tue Apr  2 18:03:21 PDT 2013 - Fixed the problem in iget().. still segmentation fault
+Wed Apr  3 14:16:01 PDT 2013 - Created alloc/dealloc functions
--- Source/cmds.c
+++ Source/cmds.c
@@ -5,6 +5,7 @@
 //	Command Functions
 //
 ////////////////////////////////////////////////////////
+extern int dev;
 
 int _menu(char *pathname)
 {
@@ -35,9 +36,62 @@ int _cd(char *pathname)
 
 int _mkdir(char *pathname)
 {
+	char * parent, * child;
+	char fullpath[128], fullpath2[128];
+
+	unsigned long ino;
+	MINODE * pip;
+
+	// Create a copy of the pathname so we don't alter it directly
+	strcpy(fullpath, pathname);
+	strcpy(fullpath2, pathname);
+
 	printf("~~~~~~MKDIR~~~~~~~\n\n");
-	
-	
+	/*  1. Ask for a pahtname, e.g. /a/b/c  or a/b/c, etc.
+
+		2.  if (pathname[0] == '/') 
+		        dev = root->dev;
+		     else
+		        dev = cwd->dev;
+		3. Let  
+		     parent = dirname(pathname);   parent= "/a/b"
+		     child  = basename(pathname);  child = "c"
+
+		4. Get the In_MEMORY minode of parent:
+
+		         ino  = getino(&dev, parent);
+		         pip  = iget(dev, ino); 
+
+		   Then, verify :
+		         parent INODE is a DIR AND
+		         child des NOT exists in the parent directory;
+		               
+		5. Call 
+		          r = my_mkdir(pip, child);
+		 
+		6.  return(r);*/
+
+		if(pathname[0] == '/'){
+			//dev = root->dev;
+		}else{
+			//dev = cwd->dev;
+		}
+		printf("fullPath: %s\n", fullpath);
+		parent = dirname(fullpath); 
+		child  = basename(fullpath2);
+
+		printf("parent: %s\n", parent);
+		printf("child: %s\n", child);
+
+		ino = getino(&dev, parent);
+		pip = iget(dev, ino);
+
+		if(S_ISDIR(pip->INODE.i_mode)){
+			printf("THIS IS A DIRECTORY!\n");
+		}else{
+			printf("THIS IS NOT A DIRECTORY\n");
+		}
+
 	
 	return 0;
 }
